739365d4a7be35b54c88dee32437eee6e3617dd5
Joshua Phillips <21149530+JoshuaAPhillips@users.noreply.github.com>
2023-04-26 17:55:58 +0100

    TypeError with iiiftest.py: suspect that numbers in the CSV file are getting piped across as string-like data rather than integers

--- manifests/manifest-factory/iiiftest.py
+++ manifests/manifest-factory/iiiftest.py
@@ -20,5 +20,43 @@ file = sys.argv[-1]
 
 data = csvreader.csvreader(file)
 
-print(data)
+# where the magic happens
 
+for idx, i in enumerate(data):
+    idx +=1
+
+    # set canvas properties
+
+    canvas = manifest.add_canvas_to_items()
+    canvas.set_id(extendbase_url="canvas/p%s"%idx)
+    canvas.set_height(data[2])
+    canvas.set_width(data[1])
+    canvas.add_label("en", data[0])
+
+    # set canvas annotations - create annopage object for each canvas
+
+    annopage = canvas.add_annotationpage_to_items()
+
+    # add annotation (i.e., facsimile taking up entire canvas) to annopage and provide information
+
+    annopage.set_id(extendbase_url="page/p%s/1"%idx)
+    annotation = annopage.add_annotation_to_items(target=canvas.id)
+    annotation.set_id(extendbase_url="annotation/p%s-image"%str(idx).zfill(4))
+    annotation.set_motivation("painting")
+    annotation.body.set_id("".join(data[3]))
+    annotation.body.set_type("Image")
+    annotation.body.set_format("image/jpeg")
+    annotation.body.set_width(data[1])
+    annotation.body.set_height(data[2])
+
+    # specify service for above canvas annotations
+
+    service = annotation.body.add_service()
+    service.set_id(data[3])
+    service.set_type("ImageService3")
+    service.set_profile("level1")
+
+# finishing touches - inspecting canvas for required and recommended properties, and saving out JSON file
+
+canvas.inspect()
+manifest.json_save()
