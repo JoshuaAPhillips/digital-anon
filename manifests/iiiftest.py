122ee4288b224c528db64f50ef1af3da357ecf35
Joshua Phillips <21149530+JoshuaAPhillips@users.noreply.github.com>
2023-04-26 17:07:58 +0100

    iiiftest.py: successfully pulling in data from csv via csvreader.py

--- manifests/iiiftest.py
+++ manifests/iiiftest.py
@@ -1,5 +1,6 @@
 # imports
 
+import sys
 from IIIFpres import iiifpapi3
 import csvreader
 
@@ -11,30 +12,13 @@ manifest.set_id(extendbase_url="manifest.json")
 manifest.add_label("en","Simple Manifest - Book")
 manifest.add_behavior("paged")
 
+# get data filename
+
+file = sys.argv[-1]
+
 # pull data from CSV
 
-data = csvreader.csvreader(data)
+data = csvreader.csvreader(file)
 
-for idx,d in enumerate(data):
-    idx+=1 
-    canvas = manifest.add_canvas_to_items()
-    canvas.set_id(extendbase_url="canvas/p%s"%idx) # in this case we use the base url
-    canvas.set_height(d[2])
-    canvas.set_width(d[1])
-    canvas.add_label("en",d[0])
-    annopage = canvas.add_annotationpage_to_items()
-    annopage.set_id(extendbase_url="page/p%s/1" %idx)
-    annotation = annopage.add_annotation_to_items(target=canvas.id)
-    annotation.set_id(extendbase_url="annotation/p%s-image"%str(idx).zfill(4))
-    annotation.set_motivation("painting")
-    annotation.body.set_id("".join(d[3:]))
-    annotation.body.set_type("Image")
-    annotation.body.set_format("image/jpeg")
-    annotation.body.set_width(d[1])
-    annotation.body.set_height(d[2])
-    s = annotation.body.add_service()
-    s.set_id(d[3])
-    s.set_type("ImageService3")
-    s.set_profile("level1")
+print(data)
 
-manifest.json_save("manifest.json")
