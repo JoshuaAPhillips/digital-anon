c2f1c229f79a968729d56bc21f7170e464612761
Joshua Phillips <21149530+JoshuaAPhillips@users.noreply.github.com>
2023-04-27 14:03:18 +0100

    sanitiser.py: sorted out data types so that santise() consistently feeds tuples into iiiftest.py

--- manifests/manifest-factory/sanitiser.py
+++ manifests/manifest-factory/sanitiser.py
@@ -7,30 +7,23 @@ import csvreader
 
 file = sys.argv[-1]
 
-data = []
-
 def sanitise(file):
+
     # pull raw data from CSV
 
     raw_data = csvreader.csvreader(file)
 
     # data cleanup - turns number-like strings into ints
-    if not raw_data:
-        print("Empty list!")
+    data = ()
+    for tuple in raw_data:
+        new_tpl = ()
+        for item in tuple:
+            if item.isdigit():
+                new_tpl += (int(item),)
+            else:
+                new_tpl += (item,)
+        data += new_tpl
 
-    else:
-
-        for tuple in raw_data:
-            new_tpl = ()
-            for item in tuple:
-                if item.isdigit():
-                    new_tpl += (int(item),)
-                elif type(item) is None:
-                    continue
-                else:
-                    new_tpl += (item,)
-        data.append(new_tpl)
-
-    print(data)
+    return data
 
 sanitise(file)
