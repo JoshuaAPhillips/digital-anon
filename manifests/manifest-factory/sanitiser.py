e9033102bdb4fd848c06383e1c6306b7e37d06c0
Joshua Phillips <21149530+JoshuaAPhillips@users.noreply.github.com>
2023-04-27 12:11:41 +0100

    Adjusted data stream for sanitiser.py

--- manifests/manifest-factory/sanitiser.py
+++ manifests/manifest-factory/sanitiser.py
@@ -7,16 +7,14 @@ import csvreader
 
 file = sys.argv[-1]
 
-# pull raw data from CSV
-
-raw_data = csvreader.csvreader(file)
-
-# data cleanup
-
 data = []
 
-def sanitise(raw_data):
+def sanitise(file):
+    # pull raw data from CSV
 
+    raw_data = csvreader.csvreader(file)
+
+    # data cleanup - turns number-like strings into ints
     if not raw_data:
         print("Empty list!")
 
@@ -27,10 +25,12 @@ def sanitise(raw_data):
             for item in tuple:
                 if item.isdigit():
                     new_tpl += (int(item),)
+                elif type(item) is None:
+                    continue
                 else:
                     new_tpl += (item,)
         data.append(new_tpl)
 
     print(data)
 
-sanitise(raw_data)
+sanitise(file)
