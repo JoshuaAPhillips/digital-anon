58ba763abc81070eea05071fd8bcfca11cfb5ddd
Joshua Phillips <21149530+JoshuaAPhillips@users.noreply.github.com>
2023-04-27 15:32:31 +0100

    iiiftest.py: took some fiddling but seem to have got IIIFtest.py to spit out a manifest with correct number of correct pages

--- manifests/manifest-factory/iiiftest.py
+++ manifests/manifest-factory/iiiftest.py
@@ -3,61 +3,66 @@
 import sys
 from IIIFpres import iiifpapi3
 import csvreader
+import sanitiser
 
 # base manifest setup
 
 iiifpapi3.BASE_URL = "https://iiif.io/api/cookbook/recipe/0009-book-1/"
 manifest = iiifpapi3.Manifest()
 manifest.set_id(extendbase_url="manifest.json")
-manifest.add_label("en","Simple Manifest - Book")
+manifest.add_label("en","M48 test manifest")
 manifest.add_behavior("paged")
 
 # get data filename
 
 file = sys.argv[-1]
 
-# pull data from CSV
+# pull data from CSV and sanitises
 
-data = csvreader.csvreader(file)
+data = sanitiser.sanitise(file)
 
 # where the magic happens
 
-for idx, i in enumerate(data):
-    idx +=1
+if data is not None:
 
-    # set canvas properties
+    for idx, i in enumerate(data):
 
-    canvas = manifest.add_canvas_to_items()
-    canvas.set_id(extendbase_url="canvas/p%s"%idx)
-    canvas.set_height(data[2])
-    canvas.set_width(data[1])
-    canvas.add_label("en", data[0])
+        # set canvas properties
 
-    # set canvas annotations - create annopage object for each canvas
+        canvas = manifest.add_canvas_to_items()
+        canvas.set_id(extendbase_url="canvas/p%s"%idx)
+        canvas.set_height(data[idx][2])
+        canvas.set_width(data[idx][1])
+        canvas.add_label("en", data[idx][0])
 
-    annopage = canvas.add_annotationpage_to_items()
+        # set canvas annotations - create annopage object for each canvas
 
-    # add annotation (i.e., facsimile taking up entire canvas) to annopage and provide information
+        annopage = canvas.add_annotationpage_to_items()
 
-    annopage.set_id(extendbase_url="page/p%s/1"%idx)
-    annotation = annopage.add_annotation_to_items(target=canvas.id)
-    annotation.set_id(extendbase_url="annotation/p%s-image"%str(idx).zfill(4))
-    annotation.set_motivation("painting")
-    annotation.body.set_id("".join(data[3]))
-    annotation.body.set_type("Image")
-    annotation.body.set_format("image/jpeg")
-    annotation.body.set_width(data[1])
-    annotation.body.set_height(data[2])
+        # add annotation (i.e., facsimile taking up entire canvas) to annopage and provide information
 
-    # specify service for above canvas annotations
+        annopage.set_id(extendbase_url="page/p%s/1"%idx)
+        annotation = annopage.add_annotation_to_items(target=canvas.id)
+        annotation.set_id(extendbase_url="annotation/p%s-image"%str(idx).zfill(4))
+        annotation.set_motivation("painting")
+        annotation.body.set_id(data[idx][3])
+        annotation.body.set_type("Image")
+        annotation.body.set_format("image/jpeg")
+        annotation.body.set_width(data[idx][1])
+        annotation.body.set_height(data[idx][2])
 
-    service = annotation.body.add_service()
-    service.set_id(data[3])
-    service.set_type("ImageService3")
-    service.set_profile("level1")
+        # specify service for above canvas annotations
 
-# finishing touches - inspecting canvas for required and recommended properties, and saving out JSON file
+        service = annotation.body.add_service()
+        service.set_id(data[idx][3])
+        service.set_type("ImageService3")
+        service.set_profile("level1")
 
-canvas.inspect()
-manifest.show_errors_in_browser()
-#manifest.json_save()
+    # finishing touches - inspecting canvas for required and recommended properties, and saving out JSON file
+
+    canvas.inspect()
+    manifest.show_errors_in_browser()
+    #manifest.json_save('m48-factory-test-2.json')
+      
+else:
+    print("Data is None :()")
