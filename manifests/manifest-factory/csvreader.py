2a97d47f088aa53f7cbd5205e46ddabeeb5669c8
Joshua Phillips <21149530+JoshuaAPhillips@users.noreply.github.com>
2023-04-27 14:01:40 +0100

    csvreader.py: preflight tweaks

--- manifests/manifest-factory/csvreader.py
+++ manifests/manifest-factory/csvreader.py
@@ -8,19 +8,22 @@ import csv
 
 file = sys.argv[-1]
 
-# save data as tuple
+# save raw data as list
 
 def csvreader(file, dialect='excel'):
-    data = []
+    raw_data = []
+
     try:
         with open(file, 'r') as csv_file:
             reader = csv.reader(csv_file)
             for row in reader:
-                data.append(tuple(row))
-        return data
+                raw_data.append(tuple(row))
+        return raw_data
+    
     except FileNotFoundError:
         print(f"Error: File {file} not found.")
         sys.exit(1)
+
     except csv.Error as e:
         print(f"Error reading CSV file {file}: {e}")
         sys.exit(1)
