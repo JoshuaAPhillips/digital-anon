7e845351cd1a1fbcdef5849ff74f220903ef8f8a
JoshuaAPhillips <joshua.phillips2@googlemail.com>
2023-04-30 00:59:07 +0100

    csvwriter.py: seems to be working :) will test pipeline later

--- manifests/manifest-factory/csvwriter.py
+++ manifests/manifest-factory/csvwriter.py
@@ -1,25 +1,43 @@
-import os
 import csv
 
+# requests inputs
+
 max = int(input("Please enter the number of pages for this document: "))
-length = range(max)
 doc = input("Please enter the m-number for this document: ")
 
-def csvwriter():
+# sets constants
 
-  with open("{}-data.csv".format(doc), "x") as csv_file:
+w = [2766 for i in range(max)]
+
+h = [3500 for i in range(max)]
+
+service = ["full/max/0/default.tif" for i in range(max)]
+
+# creates variables
+
+base = [f"https://llkn576nqxepo6yucgfh2nl2ea0kkigc.lambda-url.eu-west-2.on.aws/iiif/2/{doc}/{doc}-{i+1}" for i in range(max)]
+
+# print(base)
+
+folios = []
+half_max = int(max / 2)
+
+for i in range(1, half_max +1):
+    folios.append(f"{i} recto")
+    folios.append(f"{i} verso")
+
+# print(pages)
+
+rows = [folios, w, h, base, service]
+print(rows)
+# find a way of accessing the nth item from each list
+
+def csvwriter(rows):
+
+  with open(f"{doc}-data.csv", "x") as csv_file:
     writer = csv.writer(csv_file, delimiter=',')
-    
-    for idx, i in enumerate(length):
+    for idx in range(len(rows[0])):
+      row = [inner_list[idx] for inner_list in rows]
+      writer.writerow(row)
 
-
-        folio = 0
-        w = 2766
-        h = 3500
-        base = f"https://llkn576nqxepo6yucgfh2nl2ea0kkigc.lambda-url.eu-west-2.on.aws/iiif/2/{doc}/{doc}-{i + 1}"
-        service = "full/max/0/default.tif"
-
-        newdata = [folio, w, h, base, service]
-        writer.writerow(newdata)
-
-csvwriter()
+csvwriter(rows)
