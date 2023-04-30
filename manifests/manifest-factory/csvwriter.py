ee73595938429a72389e278a0bc436601fd5d87e
JoshuaAPhillips <joshua.phillips2@googlemail.com>
2023-04-30 21:18:24 +0100

    csvwriter.py: not sure what the change is here...

--- manifests/manifest-factory/csvwriter.py
+++ manifests/manifest-factory/csvwriter.py
@@ -15,26 +15,26 @@ service = ["full/max/0/default.tif" for i in range(max)]
 
 # creates variables
 
-base = [f"https://llkn576nqxepo6yucgfh2nl2ea0kkigc.lambda-url.eu-west-2.on.aws/iiif/2/{doc}/{doc}-{i+1}" for i in range(max)]
+base = [f"https://llkn576nqxepo6yucgfh2nl2ea0kkigc.lambda-url.eu-west-2.on.aws/iiif/2/m{doc}/m{doc}-{i+1}" for i in range(max)]
 
 # print(base)
 
 folios = []
 half_max = int(max / 2)
 
-for i in range(1, half_max +1):
+for i in range(1, half_max +2):
     folios.append(f"{i} recto")
     folios.append(f"{i} verso")
 
 # print(pages)
 
 rows = [folios, w, h, base, service]
-print(rows)
+# print(rows)
 # find a way of accessing the nth item from each list
 
 def csvwriter(rows):
 
-  with open(f"{doc}-data.csv", "x") as csv_file:
+  with open(f"m{doc}-data.csv", "x") as csv_file:
     writer = csv.writer(csv_file, delimiter=',')
     for idx in range(len(rows[0])):
       row = [inner_list[idx] for inner_list in rows]
