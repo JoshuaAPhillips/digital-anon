21e2156adfc82d34d316cf6c68ac16a60f7c3d22
JoshuaAPhillips <joshua.phillips2@googlemail.com>
2023-05-01 13:32:20 +0100

    csvwriter.py: update to wrap in a class

--- manifests/manifest-factory/csvwriter.py
+++ manifests/manifest-factory/csvwriter.py
@@ -1,43 +1,39 @@
 import csv
 
-# requests inputs
+class Document:
+    
+    def __init__(self, max_pages, doc_number):
+        self.max_pages = max_pages
+        self.doc_number = doc_number
 
-max = int(input("Please enter the number of pages for this document: "))
-doc = input("Please enter the m-number for this document: ")
+        # sets constants
 
-# sets constants
+        self.widths = [2766 for i in range(max_pages)]
+        self.heights = [3500 for i in range(max_pages)]
+        self.services = ["full/max/0/default.tif" for i in range(max_pages)]
 
-w = [2766 for i in range(max)]
+        # sets base URI
 
-h = [3500 for i in range(max)]
+        self.base_urls = [f"https://llkn576nqxepo6yucgfh2nl2ea0kkigc.lambda-url.eu-west-2.on.aws/iiif/2/m{doc_number}/m{doc_number}-{i + 1}" for i in range(max_pages)]
 
-service = ["full/max/0/default.tif" for i in range(max)]
+        # sets foliation
 
-# creates variables
+        self.folios = []
+        half_max = int(max_pages / 2)
 
-base = [f"https://llkn576nqxepo6yucgfh2nl2ea0kkigc.lambda-url.eu-west-2.on.aws/iiif/2/m{doc}/m{doc}-{i+1}" for i in range(max)]
+        for i in range(1, half_max + 2):
+            self.folios.append(f"{i} recto")
+            self.folios.append(f"{i} verso")
+            
+        self.rows = [self.folios, self.widths, self.heights, self.base_urls, self.services]
 
-# print(base)
+    def write_csv(self):
+        with open(f"m{self.doc_number}-data.csv", "x") as csv_file:
+            writer = csv.writer(csv_file, delimiter=',')
+            for idx in range(len(self.rows[0])):
+                row = [inner_list[idx] for inner_list in self.rows]
+                writer.writerow(row)
 
-folios = []
-half_max = int(max / 2)
 
-for i in range(1, half_max +2):
-    folios.append(f"{i} recto")
-    folios.append(f"{i} verso")
-
-# print(pages)
-
-rows = [folios, w, h, base, service]
-# print(rows)
-# find a way of accessing the nth item from each list
-
-def csvwriter(rows):
-
-  with open(f"m{doc}-data.csv", "x") as csv_file:
-    writer = csv.writer(csv_file, delimiter=',')
-    for idx in range(len(rows[0])):
-      row = [inner_list[idx] for inner_list in rows]
-      writer.writerow(row)
-
-csvwriter(rows)
+m47 = Document(4, 47)
+m47.write_csv()
---
+++ manifests/manifest-factory/csvwriter_old.py
@@ -0,0 +1,43 @@
+import csv
+
+# requests inputs
+
+max = int(input("Please enter the number of pages for this document: "))
+doc = input("Please enter the m-number for this document: ")
+
+# sets constants
+
+w = [2766 for i in range(max)]
+
+h = [3500 for i in range(max)]
+
+service = ["full/max/0/default.tif" for i in range(max)]
+
+# creates variables
+
+base = [f"https://llkn576nqxepo6yucgfh2nl2ea0kkigc.lambda-url.eu-west-2.on.aws/iiif/2/m{doc}/m{doc}-{i+1}" for i in range(max)]
+
+# print(base)
+
+folios = []
+half_max = int(max / 2)
+
+for i in range(1, half_max +2):
+    folios.append(f"{i} recto")
+    folios.append(f"{i} verso")
+
+# print(pages)
+
+rows = [folios, w, h, base, service]
+# print(rows)
+# find a way of accessing the nth item from each list
+
+def csvwriter(rows):
+
+  with open(f"m{doc}-data.csv", "x") as csv_file:
+    writer = csv.writer(csv_file, delimiter=',')
+    for idx in range(len(rows[0])):
+      row = [inner_list[idx] for inner_list in rows]
+      writer.writerow(row)
+
+csvwriter(rows)
