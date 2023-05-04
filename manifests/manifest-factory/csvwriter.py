3b684dacff2c7078ff01f0a2857308513a07edb2
Joshua Phillips <21149530+JoshuaAPhillips@users.noreply.github.com>
2023-05-04 14:47:32 +0100

    csvwriter.py: adding thumbnail detail generator

--- manifests/manifest-factory/csvwriter.py
+++ manifests/manifest-factory/csvwriter.py
@@ -24,8 +24,10 @@ class Document:
         for i in range(1, half_max + 2):
             self.folios.append(f"{i} recto")
             self.folios.append(f"{i} verso")
+
+        self.thumbs = [f"https://llkn576nqxepo6yucgfh2nl2ea0kkigc.lambda-url.eu-west-2.on.aws/iiif/2/m{doc_number}/thumbs/thumb_m{doc_number}-{i + 1}" for i in range(max_pages)]
             
-        self.rows = [self.folios, self.widths, self.heights, self.base_urls, self.services]
+        self.rows = [self.folios, self.widths, self.heights, self.base_urls, self.services, self.thumbs]
 
         print(self.max_pages)
         print(self.doc_number)
@@ -34,7 +36,7 @@ class Document:
         print(f"Output: {self.doc_number} saved... \n")
 
     def write_csv(self):
-        with open(f"./data/m{self.doc_number}-data.csv", "x") as csv_file:            
+        with open(f"./data/m{self.doc_number}-data.csv", "w") as csv_file:
             print(f"{self.doc_number} saved to CSV... \n")
             writer = csv.writer(csv_file, delimiter=',')
             for idx in range(len(self.rows[0])):
@@ -44,6 +46,6 @@ class Document:
     def __del__(self):
         print(f"{self.doc_number} destroyed")
 
-newdoc = Document(3, "1-8-10")
+newdoc = Document(10, "113")
 newdoc.write_csv()
 del(newdoc)
