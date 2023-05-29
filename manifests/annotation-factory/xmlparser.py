e5ec0f2606e4fd80b462f925c6a9228d0b662b53
Joshua Phillips <21149530+JoshuaAPhillips@users.noreply.github.com>
2023-05-29 18:17:18 +0100

    m1-8-1 and -3: rationalising tag structure for marginal additions

--- manifests/annotation-factory/xmlparser.py
+++ manifests/annotation-factory/xmlparser.py
@@ -47,6 +47,60 @@ class xmlParser:
     all_divs = soup.find_all('div')
     return all_divs
 
+  def facsDict(self):
 
+    """
+    returns a dictionary of facs attributes arranged by div
+    """
+
+    all_divs = self.allDivs()
+
+    facs_dict = {}
+    for div in all_divs:
+        parent_facs = div.get('facs')
+        children = div.find_all(attrs={'facs': True})
+        
+        child_facs_list = []
+        
+        for child in children:
+            child_facs_list.append(child.get('facs'))
+        
+        facs_dict[parent_facs] = child_facs_list
+    return facs_dict
+
+  """def childDict(self):
+      
+      
+      returns a dictionary of child attributes arranged by div --- doesn't work...
+      
+      all_divs = self.allDivs()
+
+      n = [div.get("n") for div in all_divs]
+      pp(n)
+
+      for div in all_divs:
+         child_sublist = []
+         for child in div:
+            sub_child = str(child).strip()
+            child_sublist.append(sub_child)
+
+      child_dict = dict(zip(n, child_sublist))
+      return child_dict
+"""
+  def toJson(self):
+     
+     """
+     saves dicts to Json
+     """
+     facs_dict = self.facsDict()
+     child_dict = self.childDict()
+
+     with open ('./annodata/{}-facs.json'.format(idno.strip('.xml')), 'w') as json_file:
+        json.dump(facs_dict, json_file, indent=4)
+
+     """
+     with open ('./annodata/{}-children.json'.format(idno.strip('.xml')), 'w') as json_file:
+        json.dump(child_dict, json_file, indent=4)
+     """
 test = xmlParser()
-test.makeSoup()
+test.toJson()
